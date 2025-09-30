8ebc5c9d0a32e8b6539c07894b2c642ea1e69b8d
Subash Singh Dhami <subashdhamee@gmail.com>
2025-09-30 11:56:38 +0545

    add seo head component

---
+++ components/seo-head.tsx
@@ -0,0 +1,45 @@
+import Script from "next/script"
+
+interface SEOHeadProps {
+  structuredData?: object
+  noIndex?: boolean
+}
+
+export function SEOHead({ structuredData, noIndex = false }: SEOHeadProps) {
+  return (
+    <>
+      {/* Structured Data */}
+      {structuredData && (
+        <Script
+          id="structured-data"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(structuredData),
+          }}
+        />
+      )}
+
+      {/* No Index for development/staging */}
+      {noIndex && <meta name="robots" content="noindex, nofollow" />}
+
+      {/* Preconnect to external domains */}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+      {/* DNS Prefetch */}
+      <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+      <link rel="dns-prefetch" href="//fonts.gstatic.com" />
+
+      {/* Favicon */}
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+
+      {/* Theme Color */}
+      <meta name="theme-color" content="#0f172a" />
+      <meta name="msapplication-TileColor" content="#0f172a" />
+    </>
+  )
+}
